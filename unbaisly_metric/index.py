5af221183642cfeead68196179cd1eb1f589bfb0
abhijnamaiya <abhijnamaiya5533@gmail.com>
2024-11-09 19:12:59 -0700

    Metric Evaluation - TARS Classifier

---
+++ unbaisly_metric/index.py
@@ -0,0 +1,42 @@
+from flair.models import TARSClassifier
+from flair.data import Sentence
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+classes = ["race bias","gender bias", "ethnicity bais", "sex bais", "religion bais", "income bais", 
+           "gender stereotype","race stereotype","ethnicity stereotype","sex stereotype","religion stereotype","income stereotype",
+           "social bias", "social stereotype", "economic bias", "economic stereotype", "occupation bias", "occupation stereotype",
+           "age bias", "age stereotype", "disability bias", "disability stereotype", "socioeconomic bias", "socioeconomic stereotype",
+           "LGBTQ bias", "LGBTQ stereotype", "weight bias", "weight stereotype", "class bias", "class stereotype"
+        ]
+tars = TARSClassifier.load('tars-base')
+
+# Define a POST endpoint
+@app.route('/api/baised-metric', methods=['POST'])
+def receive_data() -> dict:
+    # Get JSON data from the request
+    data = request.json['data']
+
+    lines = data.split(".")
+    result = {}
+    countMap = {}
+
+    for line in lines:
+        if line:
+            sentence = Sentence(line)
+            tars.predict_zero_shot(sentence, classes)
+            for obj in sentence.get_labels():
+                result[obj.value] = result.get(obj.value, 0) + obj.score
+                countMap[obj.value] = countMap.get(obj.value, 0) + 1
+
+    for key, value in result.items():
+        result[key] = str(round((value/countMap[key])*100, 2)) + "%"
+            
+
+    return jsonify(result), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
